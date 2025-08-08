4e66ba367981cc36df0b79d36a5eb658e9d50f0e
Munde Immaculate <immaculatemunde1@gmail.com>
2025-08-09 01:11:00 +0300

    Fix routing issues in the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard';
-import NotesPage from './pages/NotesPage';
-import PlannerPage from './pages/PlannerPage';
-import ResourcesPage from './pages/ResourcesPage';
-import ChatPage from './pages/ChatPage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-import './App.css'
+import Notes from './pages/Notes';
+import Planner from './pages/Planner';
+import Resources from './pages/Resources';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { NotesProvider } from './contexts/NotesContext';
 
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/notes" element={<NotesPage />} />
-          <Route path="/planner" element={<PlannerPage />} />
-          <Route path="/resources" element={<ResourcesPage />} />
-          <Route path="/chat" element={<ChatPage />} /> {/* optional */}
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <ThemeProvider>
+      <NotesProvider>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/notes" element={<Notes />} />
+              <Route path="/planner" element={<Planner />} />
+              <Route path="/resources" element={<Resources />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </NotesProvider>
+    </ThemeProvider>
   );
 }
